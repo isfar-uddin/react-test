0bab37c536c9d532dbbc07f1a50648241d13b185
isfar_uddin <isfar.uddin@goava.com>
2022-10-16 21:52:28 +0600

    Add link test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,15 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/* test("renders learn react link 2", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+}); */
+
+test('url is correct', () => {
+  render(<App />);
+  const linkElement = screen.getByRole('link', {name: 'Learn React'});
+  expect(linkElement.getAttribute("href")).toContain("reactjs.org");
+})
